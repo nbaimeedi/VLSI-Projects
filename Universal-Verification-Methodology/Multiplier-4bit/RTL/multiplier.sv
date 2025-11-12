db4ae84c129915e2ef4cfd0bf503508f8bd7a42e
unknown <nbaimeedi@ucsd.edu>
2025-11-12 13:29:49 +0530

    Adding Multiplier Design

---
+++ Universal-Verification-Methodology/Multiplier-4bit/RTL/multiplier.sv
@@ -0,0 +1,14 @@
+//Latest Update Date: 11th Nov, 2025
+//Owner: B Nithin Reddy
+
+
+//Multiplier Design
+
+module mul(
+  input [3:0] a,b,
+  output [7:0] y
+);
+  
+  assign y = a * b;
+  
+endmodule
