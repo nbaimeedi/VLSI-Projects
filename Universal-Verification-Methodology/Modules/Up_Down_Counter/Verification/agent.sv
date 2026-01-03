c07bb4c4a01247011fce0dc76e393549803fb22b
unknown <nitinreddy255@gmail.com>
2026-01-03 14:34:19 +0530

    Adding Code for the Agent

---
+++ Universal-Verification-Methodology/Modules/Up_Down_Counter/Verification/agent.sv
@@ -0,0 +1,26 @@
+//Agent Class
+class agent extends uvm_agent;
+  
+  `uvm_component_utils(agent)
+  
+  driver drv;
+  monitor mon;
+  uvm_sequencer #(transaction) seqr;
+  
+  function new(string name = "path", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+  
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    drv = driver::type_id::create("drv",this);
+    mon = monitor::type_id::create("mon",this);
+    seqr = uvm_sequencer#(transaction)::type_id::create("seqr",this);
+  endfunction
+  
+  virtual function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    drv.seq_item_port.connect(seqr.seq_item_export);
+  endfunction
+  
+endclass
