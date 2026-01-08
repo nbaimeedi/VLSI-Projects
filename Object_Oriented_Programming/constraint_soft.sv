c30aef10b59bf619504ee10d13cf8d6ca3803812
unknown <nitinreddy255@gmail.com>
2026-01-08 12:11:32 +0530

    Adding Code for Soft Constraint

---
+++ Object_Oriented_Programming/constraint_soft.sv
@@ -0,0 +1,43 @@
+class first;
+  
+  rand int d;
+  
+  constraint d_constraint {soft d inside {[10:20]};}
+  
+endclass
+
+
+module tb;
+  
+  first f1;
+  first f2;
+  
+  initial begin
+    f1 = new();
+    repeat(5) begin
+      assert(f1.randomize());
+      $display("Parent: data = %0d",f1.d);
+    end
+    //Output:
+    //Parent: data = 17
+    //Parent: data = 19
+    //Parent: data = 15
+    //Parent: data = 16
+    //Parent: data = 20
+    //i.e., parent class constraint is followed
+    
+    f2 = new();
+    repeat(5) begin
+      f2.randomize with {f2.d inside {[30:40]};};
+      $display("Parent: data = %0d",f2.d);
+    end
+    //Output:
+    //Parent: data = 36
+    //Parent: data = 38
+    //Parent: data = 31
+    //Parent: data = 35
+    //Parent: data = 37
+    //This is because the in-line constraint overrides the soft constraint in the parent class
+  end
+  
+endmodule
